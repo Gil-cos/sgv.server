39558aa9c8fc1ed106f05a9d30385de5ab0584d3
gilbjuni <gilberto.junior@gsw.com.br>
2021-03-02 20:31:20 -0300

    initial commit

---
+++ src/test/java/br/com/gilberto/sgv/server/SgvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gilberto.sgv.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
